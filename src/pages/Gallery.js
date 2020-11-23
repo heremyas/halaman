a5486527594205cb516d011ecceae4cdec962f4b
heremyas <jhem2911@gmail.com>
2020-11-23 10:42:50 +0800

    calling api

--- src/pages/Gallery.js
+++ src/pages/Gallery.js
@@ -4,9 +4,9 @@ import "./Gallery.css";
 const Gallery = () => {
   useEffect(() => {
     test();
-  });
+  }, []);
 
-  const [state, setstate] = useState(null);
+  const [state, setstate] = useState('');
   const url =
     "https://trefle.io/api/v1/plants/search?token=RFn2dI7Xud0AVrGPBmpFpaDdFDnrdv_fr-dsjT8KyGA&q=succulent";
   //   const url = "https://jemtech-api.herokuapp.com/api/content_data/all";
@@ -15,16 +15,14 @@ const Gallery = () => {
     const response = await fetch(url);
     const data = await response.json();
     setstate(data.data);
-    console.log(state);
   };
 
   return (
     <div id="gallery" className="container">
       <div className="images">
-        {/* {console.log(state)} */}
-        {/* {data.map((images) => (
-          <img key={} src={im}></img>
-        ))} */}
+        {state.map((images) => (
+          <img key={images.id} src={images.image_url} alt={images.common_name}></img>
+        ))}
       </div>
     </div>
   );
